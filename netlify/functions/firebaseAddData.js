fceaf9de30b61fc9d12c6106aac81cef362681b0
WMB Admin <132984570+wemetbefore@users.noreply.github.com>
2025-01-27 20:51:13 +0200

    update firebase

--- netlify/functions/firebaseAddData.js
+++ netlify/functions/firebaseAddData.js
@@ -1,6 +1,24 @@
 const { db } = require("./firebase");
 
+function getCorsHeaders(origin) {
+    return {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Accept, Origin',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Max-Age': '86400',
+        'Content-Type': 'application/json'
+    }
+}
+
 exports.handler = async (event) => {
+
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: getCorsHeaders(event.headers.origin)
+        };
+    }
+
     try {
         const data = JSON.parse(event.body);
 
