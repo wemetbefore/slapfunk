742e1385dc8a5de1f82d0bb9d92108f70103ec83
WMB Admin <132984570+wemetbefore@users.noreply.github.com>
2025-01-28 10:44:26 +0200

    updates

--- netlify/functions/firebaseAddData.js
+++ netlify/functions/firebaseAddData.js
@@ -1,6 +1,22 @@
 const { db } = require("./firebase");
 
-exports.handler = async () => {
+function getCorsHeaders(origin) {
+    return {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Accept, Origin',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Max-Age': '86400',
+        'Content-Type': 'application/json'
+    }
+}
+
+exports.handler = async (event) => {
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: getCorsHeaders(event.headers.origin)
+        };
+    }
     try {
         const snapshot = await db.collection("subscriptions").get();
         const data = snapshot.docs.map((doc) => doc.data());
